8665e8e02c3ff833199c5cb0a1f079c65f431215
Hayat ilyas <ihayat855@gmail.com>
2024-12-30 17:25:46 +0530

    Initialize the repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+    return (
+        <main className="">
+            <h2>Happy new years</h2>
+            <p>Lorem</p>
+            <button className="h-10  rounded-md py-1 px-2 bg-gray-700
+                text-white">
+                Click Here
+            </button>
+        </main>
+    )
+}
+
+export default App
